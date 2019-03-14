49d1346ee503fe60a9728f6be13c7e4810078842
Nilavan Rajamani <nilavanrajamani@gmail.com>
2019-03-14 09:13:15 +0530

    Initial commit

---
+++ SimpleClasses/Account.h
@@ -0,0 +1,14 @@
+﻿#include <vector>
+#include <string>
+#include "Transaction.h"
+
+class Account {
+private:
+	int balance;
+	std::vector<Transaction> log;
+public:
+	Account();
+	std::vector<std::string> Report();
+	bool Deposit(int amt);
+	bool Withdraw(int amt);
+};
--- SimpleClasses/SimpleClasses.cpp
+++ SimpleClasses/SimpleClasses.cpp
@@ -1,21 +1,28 @@
 // SimpleClasses.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include "pch.h"
 #include <iostream>
+#include "Account.h"
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	Account a1;
+	a1.Deposit(90);
+	cout << "After depositing 90" << endl;
+	for (auto s : a1.Report()) {
+		cout << s << endl;
+	}
+	a1.Withdraw(50);
+	if (a1.Withdraw(100))
+	{
+		cout << "second withdraw succeeds" << endl;
+	}
+	cout << "After withdrawing $50 then $100" << endl;
+	for (auto s : a1.Report())
+	{
+		cout << s << endl;
+	}
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
